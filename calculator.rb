d7c428158e5a05fe1b4978135a33676b8de00ef7
Koji Yoshida <k.yoshida@gmail.com>
2015-07-03 10:53:50 -0700

    formatting changes

--- calculator.rb
+++ calculator.rb
@@ -1,28 +1,30 @@
 # calculator.rb
+
 require 'pry'
-def calc_me(a, b)
-  yield(a, b)
+
+def calc_me(a, b, c)
+  result = yield(a, b)
+  puts "#{c} = #{result}"
 end
 
 puts "enter calculation (ex: 1 + 2)"
 calc_str = gets.chomp
 # puts calc
 
-# /(\d)([\+\-\*\\\)(\d)/.match(calc_str)
 nums = /(\d+)\s*([\+\-\*\/])\s*(\d+)/.match(calc_str)
 puts nums.inspect
 
 # binding.pry
 if nums
-case nums[2]
+  case nums[2]
   when '+'
-    calc_me(nums[1], nums[3]) { |a, b| puts "#{a.to_i + b.to_i}" }
+    calc_me(nums[1], nums[3], calc_str) { |a, b| a.to_i + b.to_i }
   when '-'
-    calc_me(nums[1], nums[3]) { |a, b| puts "#{a.to_i - b.to_i}" }
+    calc_me(nums[1], nums[3], calc_str) { |a, b| a.to_i - b.to_i }
   when '*'
-    calc_me(nums[1], nums[3]) { |a, b| puts "#{a.to_i * b.to_i}" }
+    calc_me(nums[1], nums[3], calc_str) { |a, b| puts a.to_i * b.to_i }
   when '/'
-    calc_me(nums[1], nums[3]) { |a, b| puts "#{a.to_f / b.to_i}" }
-end
+    calc_me(nums[1], nums[3], calc_str) { |a, b| puts a.to_f / b.to_i }
+  end
 end
 # binding.pry
