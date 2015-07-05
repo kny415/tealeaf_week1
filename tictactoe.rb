a4c60db31602d35efd488405b1ed9389f46335ea
Koji Yoshida <k.yoshida@gmail.com>
2015-07-05 09:29:49 -0700

    cleaned up notes in code

--- tictactoe.rb
+++ tictactoe.rb
@@ -8,37 +8,17 @@
 
 require 'pry'
 
-# BOARD_SIZE = 3
-# $player_score = []
-
-# def init_board
-#   board = {}
-#   (1..9).each { |k| board[k] = ' '}
-#   board
-# end
-
 def init_board(size)
   board = {}
   (1..(size*size)).each { |k| board[k] = ' '}
   board
-  # binding.pry
 end
 
 def init_scores(size)
   player_score = []
   (0..(size*size-1)).each { |i| player_score[i] = 0 }
-  # binding.pry
   player_score
 end
-
-# def draw_board(b)
-#   system 'clear'
-#   puts " #{b[1]} | #{b[2]} | #{b[3]} "
-#   puts "------------"
-#   puts " #{b[4]} | #{b[5]} | #{b[6]} "
-#   puts "------------"
-#   puts " #{b[7]} | #{b[8]} | #{b[9]} "
-# end
 
 def draw_board(b)
   system 'clear'
@@ -56,46 +36,15 @@ def draw_board(b)
   print "\n\n"
 end
 
-# mod 3 gives column,  0, 1, 2, 0, 1, 2, 0, 1, 2
-# /3 gives row,               0, 0, 0, 1, 1, 1, 2, 2, 2
-# 012
-# 345
-# 678
-# row1 = index/3  = 0, score[index/3] += 1
-# row2 = index/3 = 1
-# row3 = index/3 = 2
-# col1 = index%3 = 0, score[BOARD_SIZE+index/3] += 1
-# col2 = index%3 = 1
-# col3 = index%3 = 2
-# diag1 = 0, 4, 8: 0..(n-1)*(n+1)
-# diag1 = index%4 = 0, if index%4 == 0, score[n*2] += 1
-# diag2 = 2, 4, 6: 1..(n)*(n-1)
-# diag2 = index/2 = 0, if (index < n*n-1) and (index/2 == 0), score[n*2+1] += 1
-
-# 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3
-# 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2
-
-# diag1 = 0, 5, 10, 15:  0..(n-1)*(n+1)
-# diag2 = 3, 6, 9, 12:  1..n*(n-1)
-# 0123
-# 4567
-# 891011
-# 12131415 
-# diag1 = index%5 = 0
-# diag2 = index%3 = 0, if index < n*n-1
-
 def update_score(player_score, index, board_size)
-  # binding.pry
   player_score[index/board_size] += 1
   player_score[board_size + index % board_size] += 1
   if ( index % (board_size + 1) == 0 )
     player_score[board_size * 2] += 1
   end
-  if ((index < board_size * board_size - 1) and (index % (board_size - 1) == 0) and (index > 0))
+  if ((index < board_size * board_size - 1) && (index % (board_size - 1) == 0) && (index > 0))
     player_score[board_size * 2 + 1] += 1
   end
-  # binding.pry
-  
 end
 
 def board_full?(b)
@@ -132,6 +81,7 @@ until board_full?(board)
   draw_board(board)
   puts "enter square index (1 - #{ board_size**2 }):  q to quit"
   players_pick = gets.chomp
+  
   if ( /[1-9]/.match(players_pick) && square_not_taken?(board, players_pick.to_i))
     board[players_pick.to_i] = 'X'
     update_score(player_score, (players_pick.to_i - 1), board_size) 
@@ -152,9 +102,8 @@ until board_full?(board)
       break
     end
 
-    # binding.pry
   elsif players_pick == 'q'
     break
   end
-  # binding.pry
+
 end 
